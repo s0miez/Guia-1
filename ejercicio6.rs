8402c26b440d14f0a9978b8e33b26b8a238071fd
Felipe <71887490+s0miez@users.noreply.github.com>
2023-08-30 22:52:08 -0400

    Create ejercicio6.rs

---
+++ ejercicio6.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut numero: i32 = 11;
+    let mut completado = false;
+    while !completado {
+         numero -= 1;
+         println!("{}", numero);
+         if numero == 0 {
+            break;
+         }
+    }
+    println!()
+}
