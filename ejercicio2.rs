e4e5eec24021f0839f777fc93a4abcb0d9ca8874
Felipe <71887490+s0miez@users.noreply.github.com>
2023-09-03 17:36:51 -0300

    ejercicio2.rs

--- ejercicio2.rs
+++ ejercicio2.rs
@@ -2,8 +2,8 @@ use std::io;
 
 fn main() {
     let mut nombre = String::new();
-    let stdin = io::stdin();
     println!("Cuál es tu nombre?");
+    let stdin = io::stdin();
     stdin.read_line(&mut nombre).unwrap();
     println!("Tu nombre es: {}", nombre);
 }
