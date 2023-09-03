3020f3b726094a6b8294e722043e38d80f46f527
Felipe <71887490+s0miez@users.noreply.github.com>
2023-09-03 17:51:00 -0300

    Create ejercicio9.rs

---
+++ ejercicio9.rs
@@ -0,0 +1,29 @@
+use std::io;
+fn main() {
+    let mut num1:String = String::new();
+    println!("Ingresa un número: ");
+    let stdin = io::stdin();
+    stdin.read_line(&mut num1).unwrap();
+    let num1: u8 = num1.trim().parse().unwrap();
+
+    let mut num2:String = String::new();
+    println!("Ingresa otro número: ");
+    let stdin = io::stdin();
+    stdin.read_line(&mut num2).unwrap();
+    let num2: u8 = num2.trim().parse().unwrap();
+
+    let mut num3:String = String::new();
+    println!("Ingresa otro número: ");
+    let stdin = io::stdin();
+    stdin.read_line(&mut num3).unwrap();
+    let num3: u8 = num3.trim().parse().unwrap();
+
+    let mut num4:String = String::new();
+    println!("Ingresa otro número: ");
+    let stdin = io::stdin();
+    stdin.read_line(&mut num4).unwrap();
+    let num4: u8 = num4.trim().parse().unwrap();
+
+    let resultado: u8 = num1 * num2 * num3 * num4;
+    println!("El producto (multiplicación) de los números ingresados es: {}", resultado);
+}
