1bc2ecdda6949db91579e1a9647f8fe6a8c1f75d
Felipe <71887490+s0miez@users.noreply.github.com>
2023-09-03 17:37:57 -0300

    Update ejercicio4.rs

--- ejercicio4.rs
+++ ejercicio4.rs
@@ -1,8 +1,8 @@
 use std::io;
 fn main() {
     let mut numero:String = String::new();
-    let stdin = io::stdin();
     println!("Ingresa un número: ");
+    let stdin = io::stdin();
     stdin.read_line(&mut numero).unwrap();
     let numero: u8 = numero.trim().parse().unwrap();
     if numero % 2 == 0 {
